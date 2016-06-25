8256099e14993acd17c0aa3ebec68a2f732f9928
JendrikDerWeise <jbulk@stud.hs-bremen.de>
2016-06-25 18:01:51 +0200

    jbh

--- VaadinNEw/warteliste/src/main/java/com/example/warteliste/UIUserEditor.java
+++ VaadinNEw/warteliste/src/main/java/com/example/warteliste/UIUserEditor.java
@@ -1,5 +1,6 @@
 package com.example.warteliste;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.vaadin.data.util.BeanItemContainer;
@@ -13,18 +14,28 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 
-import backend.Child;
-import backend.ChildBackend;
+import controller.dao.factories.DAOFactory;
+import controller.dao.implementation.mysql.DAOChildSql;
+import model.Child;
 
 public class UIUserEditor extends UIUser{
+	List<Child> list = new ArrayList();
+	BeanItemContainer<Child> container;
 	
+	private DAOFactory mChildFactory = DAOFactory.getDAOFactory(DAOFactory.CHILD);
+	DAOChildSql childSQL;
 	
 	public UIUserEditor(){
+		childSQL = new DAOChildSql();
+		list = childSQL.getAllChildren();
+		container = new BeanItemContainer<>(Child.class, list);
+		
 			//Läd die Array Liste aus ChildBackend
-		   final List<Child> list= ChildBackend.getChildren();
+		   //List<Child> list= ChildBackend.getChildren();
+		   //final List<ChildForView> list= ChildBackend.getChildren();
 		   
 		   //pckt die Child Liste in einen BeanItem Container (diesen kann man dann in das Grid(artTabelle)rein packen)
-	       final BeanItemContainer<Child> container = new BeanItemContainer<>(Child.class, list);
+	        //BeanItemContainer<Child> container = new BeanItemContainer<>(Child.class, list);
 	       //verbindet Grid und Bean Item Container
 	       kindListe.setContainerDataSource(container);
 	       //entfehrnt EINE Bestimte Reihe aus der Tabelle
@@ -32,19 +43,29 @@ public class UIUserEditor extends UIUser{
 	       gender.addItems("Junge", "Mädchen", "Alle");
 	       accepted.addItems("nicht Angenommen", "Angenommen", "Alle");
 	       
+	       /*suche.addClickListener(new Button.ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				DAOKitaRegistrationSql abfrage = new DAOKitaRegistrationSql();
+				int status = (int)accepted.getValue();
+				KitaRegistration kitaRegistration = null; 
+		
+				
+			}
+		});*/
+	       
+	       
 	       //Click listener für die Kind Tabelle
 	       kindListe.addItemClickListener(new ItemClickEvent.ItemClickListener() {
 	    
 	    	   private Alignment TOP_RIGHT;
 
-			@Override
-	    	    
-	    	    public void itemClick(ItemClickEvent itemClickEvent) {
+	    	   @Override    
+	    	   public void itemClick(ItemClickEvent itemClickEvent) {
 	    		   //öffnet ein Window mit der Id des geklicken objekts
 	    	        final Window window = new Window(itemClickEvent.getItemId().toString());
-	    	        
-	    	        
-	    	        
+	    	  
 	    	        Button save = new Button("Speichern");
 	    	        save.setStyleName("friendly");
 	    	        save.addClickListener(new Button.ClickListener() {
